76362070ff479c1e67ee29846b898481c5649472
manan-m-shah <shahmananvyara01@gmail.com>
2022-03-27 22:18:54 +0530

    connected with backend

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./scss/index.scss";
 import App from "./App";
 import { AppProvider } from "./context/AppProvider";
 import { BrowserRouter } from "react-router-dom";
+import Web3Provider from "./context/Web3Provider";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
+      <Web3Provider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </Web3Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
